a654d631b97217fd10b7f6c66e4d9d0f3aae76ee
Vadim Manaenko <vadim.razorq@gmail.com>
2020-01-20 18:21:13 +0300

    Init

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ stdenv
+, mkRosPackage
+, robonomics_comm
+, python3Packages
+}:
+
+mkRosPackage rec {
+  name = "${pname}-${version}";
+  pname = "digital_passport_agent";
+  version = "0.1.0";
+
+  src = ./.;
+
+  propagatedBuildInputs = [
+    robonomics_comm
+    python3Packages.requests
+  ];
+
+  meta = with stdenv.lib; {
+    description = "Issues a digital passport based on objective data";
+    homepage = http://github.com/vourhey/digital_passport_agent;
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ vourhey ];
+  };
+}
